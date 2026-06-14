b997bd63a3f2f05c52c6a87c3add623c3da35689
Abbe Breitholtz <abbe.breitholtz@gmail.com>
2026-06-14 20:04:28 +0200

    feat: add mod skeleton and metadata

---
+++ src/main/java/com/minecraftcities/core/MinecraftCitiesCore.java
@@ -0,0 +1,15 @@
+package com.minecraftcities.core;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(MinecraftCitiesCore.MOD_ID)
+public class MinecraftCitiesCore {
+
+    public static final String MOD_ID = "minecraftcitiescore";
+
+    public MinecraftCitiesCore(IEventBus modBus, ModContainer container) {
+        // Registries and config wired here in later tasks
+    }
+}
